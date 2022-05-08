7d1746ecee37dd61618ce2c7e64674142681aacf
M-Farag <mina.farag@icloud.com>
2022-05-08 03:52:14 +0200

    ADD Pivot table Brand_User

---
+++ database/migrations/2022_05_08_014552_create_table_brand_user.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand_user', function (Blueprint $table) {
+            $table->bigInteger('brand_id');
+            $table->bigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('brand_user');
+    }
+};
